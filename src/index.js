6423823d0a5a11e9eab3bbe2ffb9ee26e227c631
Adam Greenwell <adamgreenwell@gmail.com>
2024-11-22 10:36:49 -0500

    Main block editor sidebar panel

--- src/index.js
+++ src/index.js
@@ -1,65 +1,137 @@
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/editor';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { DateTimePicker, PanelBody } from '@wordpress/components';
+import {
+	DateTimePicker,
+	PanelBody,
+	PanelRow,
+	Notice,
+	ToggleControl,
+	Flex,
+	FlexBlock,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 
 const SchocialSchedulerSidebar = () => {
-    const postId = useSelect(select => select('core/editor').getCurrentPostId());
-    const scheduleData = useSelect(select =>
-        select('core/editor').getEditedPostAttribute('meta')?._schocial_schedule
-    );
-    const { editPost } = useDispatch('core/editor');
+	const scheduleData = useSelect( select => {
+		if ( ! select( 'core/editor' ) ) {
+			console.log( 'core/editor store not found' ); // eslint-disable-line no-console
+			return {
+				platforms: {},
+				schedule: {},
+			};
+		}
 
-    const updateSchedule = (platform, datetime) => {
-        const newSchedule = {
-            ...scheduleData,
-            [platform]: datetime
-        };
+		const meta = select( 'core/editor' ).getEditedPostAttribute( 'meta' ) || {};
 
-        editPost({
-            meta: {
-                _schocial_schedule: newSchedule
-            }
-        });
-    };
+		// Ensure we have objects even if meta is undefined
+		const platforms = meta._schocial_platforms || {
+			facebook: false,
+			twitter: false,
+			linkedin: false,
+			instagram: false,
+		};
 
-    const platforms = useSelect(select => {
-        const settings = select('core/editor').getEditorSettings().schocial_settings;
-        return [
-            { id: 'facebook', label: 'Facebook' },
-            { id: 'twitter', label: 'X (Twitter)' },
-            { id: 'linkedin', label: 'LinkedIn' },
-            { id: 'instagram', label: 'Instagram' }
-        ].filter(platform => settings?.enabled_platforms?.[platform.id]);
-    });
+		const schedule = meta._schocial_schedule || {
+			facebook: null,
+			twitter: null,
+			linkedin: null,
+			instagram: null,
+		};
 
-    return (
-        <>
-            <PluginSidebar
-                name="schocial-scheduler"
-                title={__('Schocial Scheduler')}
-            >
-                {platforms.map(({ id, label }) => (
-                    <PanelBody key={id} title={label} __nextHasNoMarginBottom={true}>
-                        <DateTimePicker
-                            currentDate={scheduleData?.[id]}
-                            onChange={(date) => updateSchedule(id, date)}
-                            is12Hour={true}
-                        />
-                    </PanelBody>
-                ))}
-            </PluginSidebar>
-            <PluginSidebarMoreMenuItem
-                target="schocial-scheduler"
-            >
-                {__('Schocial Scheduler')}
-            </PluginSidebarMoreMenuItem>
-        </>
-    );
+		return { platforms, schedule };
+	}, [] );
+
+	const { editPost } = useDispatch( 'core/editor' );
+
+	const updateSchedule = ( platform, datetime ) => {
+		const newSchedule = {
+			...scheduleData.schedule,
+			[ platform ]: datetime,
+		};
+
+		editPost( {
+			meta: {
+				...scheduleData,
+				_schocial_schedule: newSchedule,
+			},
+		} );
+	};
+
+	const togglePlatform = ( platform, enabled ) => {
+		const newPlatforms = {
+			...scheduleData.platforms,
+			[ platform ]: enabled,
+		};
+
+		// If disabling, set schedule to null for this platform
+		const newSchedule = {
+			...scheduleData.schedule,
+			[ platform ]: enabled ? scheduleData.schedule[ platform ] : null,
+		};
+
+		editPost( {
+			meta: {
+				_schocial_platforms: newPlatforms,
+				_schocial_schedule: newSchedule,
+			},
+		} );
+	};
+
+	const platforms = [
+		{ id: 'facebook', label: 'Facebook' },
+		{ id: 'twitter', label: 'X (Twitter)' },
+		{ id: 'linkedin', label: 'LinkedIn' },
+		{ id: 'instagram', label: 'Instagram' },
+	];
+
+	return (
+		<>
+			<PluginSidebar
+				name="schocial-scheduler"
+				title={ __( 'Schocial Scheduler' ) }
+				icon="calendar-alt"
+			>
+				<Notice status="info" isDismissible={ false }>
+					{ __( 'Configure your social media schedule' ) }
+				</Notice>
+
+				{ platforms.map( ( { id, label } ) => (
+					<PanelBody key={ id } title={ label } initialOpen={ id === 'facebook' }>
+						<PanelRow>
+							<Flex direction="column" gap={ 4 }>
+								<ToggleControl
+									label={ `Enable ${ label } sharing` }
+									checked={ Boolean( scheduleData.platforms[ id ] ) }
+									onChange={ enabled => togglePlatform( id, enabled ) }
+								/>
+								{ scheduleData.platforms[ id ] && (
+									<FlexBlock>
+										<DateTimePicker
+											currentDate={
+												scheduleData.schedule[ id ] ? new Date( scheduleData.schedule[ id ] ) : null
+											}
+											onChange={ date => updateSchedule( id, date ) }
+											is12Hour={ true }
+										/>
+									</FlexBlock>
+								) }
+							</Flex>
+						</PanelRow>
+					</PanelBody>
+				) ) }
+			</PluginSidebar>
+			<PluginSidebarMoreMenuItem target="schocial-scheduler">
+				{ __( 'Schocial Scheduler' ) }
+			</PluginSidebarMoreMenuItem>
+		</>
+	);
 };
 
-registerPlugin('schocial-scheduler', {
-    render: SchocialSchedulerSidebar,
-    icon: 'share'
-});
+// Make sure the plugin is registered after DOM is ready
+window.addEventListener( 'DOMContentLoaded', () => {
+	registerPlugin( 'schocial-scheduler', {
+		render: SchocialSchedulerSidebar,
+		icon: 'calendar-alt',
+	} );
+} );
